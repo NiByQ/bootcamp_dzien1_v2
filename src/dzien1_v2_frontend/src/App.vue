aeab701c76db4ed2b6491dd6e8bdc2c58e0d7b0c
NiByQ <nikobycz@gmail.com>
2024-06-25 22:02:51 +0200

    mam przycisk

--- src/dzien1_v2_frontend/src/App.vue
+++ src/dzien1_v2_frontend/src/App.vue
@@ -5,5 +5,26 @@
     <br />
     <br />
     <p>hello world</p>
+    <br>
+    {{ licznik }}
+    <input type="text" id="button1">
   </main>
 </template>
+
+<script>
+let but1 = document.getElementById('button1').addEventListener('click', function(){
+  iteruj();
+})
+export default{
+  date(){
+    return{
+      licznik: 0
+    }
+  },
+  methods: {
+    iteruj() {
+      this.licznik++
+    }
+  },
+}
+</script>
